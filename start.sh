88b41c1a408198bd002231e872923f017672097d
BailangMax <langb6416@gmail.com>
2025-09-03 16:41:39 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-export UUID=${UUID:-'2fbaf3b0-093c-4706-bb38-11e21b519764'} # 哪吒v1,在不同的平台部署需要改UUID，否则会覆盖
+export UUID=${UUID:-'8784aeca-1608-4faf-b977-e4c850a1fc77'} # 哪吒v1,在不同的平台部署需要改UUID，否则会覆盖
 export NEZHA_SERVER=${NEZHA_SERVER:-'a.holoy.dpdns.org:36958'}       # v1哪吒填写形式：nezha.abc.com:8008,v0哪吒填写形式：nezha.abc.com
 export NEZHA_PORT=${NEZHA_PORT:-''}           # v1哪吒不要填写这个,v0哪吒agent端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
 export NEZHA_KEY=${NEZHA_KEY:-'NwxKJwM9UKRCX5TBPaBm0IrjNCSyflif'}             # v1的NZ_CLIENT_SECRET或v0的agent密钥
